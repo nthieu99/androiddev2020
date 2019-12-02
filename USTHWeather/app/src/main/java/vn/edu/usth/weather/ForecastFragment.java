10efb3336132b09b33cbe861dcedbd0e4a3cd484
nthieu99 <hieunt.b8067@st.usth.edu.vn>
2019-12-02 23:50:22 +0700

    Practical Work 4: DynamicViews

--- USTHWeather/app/src/main/java/vn/edu/usth/weather/ForecastFragment.java
+++ USTHWeather/app/src/main/java/vn/edu/usth/weather/ForecastFragment.java
@@ -7,16 +7,36 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
+import androidx.fragment.app.Fragment;
 
 public class ForecastFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
-        View view = new View(getContext());
-        view.setBackgroundColor(0x6080DF80);
-        return inflater.inflate(R.layout.fragment_forecast, container, false);
-    }
+        View view = inflater.inflate(R.layout.fragment_forecast, container, false);
+        view.setBackgroundColor(0xFF7b68ee);
 
+
+        LinearLayout fragment_container = view.findViewById(R.id.fragment_forecast);
+        fragment_container.setOrientation(LinearLayout.VERTICAL);
+
+        ImageView weather = new ImageView(getContext());
+        weather.setImageResource(R.drawable.sunny);
+
+        TextView day = new TextView(getContext());
+        day.setText("Thursday");
+        day.setTextSize(36);
+        day.setBackgroundColor(0xFF7b68ee);
+        day.setPadding(0, 10, 0, 10);
+
+        fragment_container.addView(weather);
+        fragment_container.addView(day);
+
+        return view;
+    }
 }
