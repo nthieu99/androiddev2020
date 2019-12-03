23479a7710d601689fe7fd23d6e223c4a49a1fe4
nthieu99 <hieunt.b8067@st.usth.edu.vn>
2019-12-03 23:22:26 +0700

    Practical Work 5: Layouts

--- USTHWeather/app/src/main/java/vn/edu/usth/weather/ForecastFragment.java
+++ USTHWeather/app/src/main/java/vn/edu/usth/weather/ForecastFragment.java
@@ -19,23 +19,7 @@ public class ForecastFragment extends Fragment {
                              Bundle savedInstanceState) {
 
         View view = inflater.inflate(R.layout.fragment_forecast, container, false);
-        view.setBackgroundColor(0xFF7b68ee);
-
-
-        LinearLayout fragment_container = view.findViewById(R.id.fragment_forecast);
-        fragment_container.setOrientation(LinearLayout.VERTICAL);
-
-        ImageView weather = new ImageView(getContext());
-        weather.setImageResource(R.drawable.sunny);
-
-        TextView day = new TextView(getContext());
-        day.setText("Thursday");
-        day.setTextSize(36);
-        day.setBackgroundColor(0xFF7b68ee);
-        day.setPadding(0, 10, 0, 10);
-
-        fragment_container.addView(weather);
-        fragment_container.addView(day);
+        view.setBackgroundColor(0xFFE0E0FF);
 
         return view;
     }
